46db0ebebbe3039442a39958f2992051dbda4b9e
심철환 <a@akaiv.com>
2016-03-12 00:06:12 +0900

    array의 연도, 설명, 코치를 가져오도록 함수 작성

--- functions.php
+++ functions.php
@@ -3,12 +3,16 @@ function video_list( $array ) {
   foreach ($array as $a) :
     echo '<h2>' . $a['동작'] . '</h2><ul class="list-inline">';
     foreach ($a['유튜브'] as $y) :
-      $title = $y['제목'];
-      $url = "https://www.youtube.com/watch?v=" . $y['주소'];
-      $img = "https://i.ytimg.com/vi_webp/" . $y['주소'] . "/mqdefault.webp"; ?>
+      $title       = $y['제목'];
+      $year        = $y['연도'];
+      $description = $y['설명'];
+      $coach       = $y['코치'];
+      $url         = "https://www.youtube.com/watch?v=" . $y['주소'];
+      $img         = "https://i.ytimg.com/vi_webp/" . $y['주소'] . "/mqdefault.webp"; ?>
       <li>
-        <a href="<?php echo $url; ?>" target="_blank"><img src="<?php echo $img; ?>"></a>
-        <h3><a href="<?php echo $url; ?>" target="_blank"><?php echo $title; ?></a></h3>
+        <a href="<?php echo $url; ?>" target="_blank"><img class="item-thumbnail" src="<?php echo $img; ?>"></a>
+        <h3 class="item-title"><a href="<?php echo $url; ?>" target="_blank"><?php echo $title; ?></a> <small><?php echo $year; ?></small></h3>
+        <p class="item-description"><?php echo $description; if ( ! empty( $coach ) ) echo ' &mdash; '.$coach; ?></p>
       </li><?php
     endforeach;
     echo '</ul>';
