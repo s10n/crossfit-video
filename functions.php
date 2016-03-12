3a37abba7383d7c5238ae721a2b641efebde14da
심철환 <a@akaiv.com>
2016-03-13 02:13:04 +0900

    비디오 목록 출력하는 함수의 각 요소를 또한 각각의 함수로 변경

--- functions.php
+++ functions.php
@@ -10,11 +10,29 @@ function video_list( $array ) {
       $url         = "https://www.youtube.com/watch?v=" . $y['주소'];
       $img         = "https://i.ytimg.com/vi/" . $y['주소'] . "/mqdefault.jpg"; ?>
       <li>
-        <a href="<?php echo $url; ?>" target="_blank"><img class="item-thumbnail" src="<?php echo $img; ?>"></a>
-        <h3 class="item-title"><a href="<?php echo $url; ?>" target="_blank"><?php echo $title; ?></a> <small><?php echo $year; ?></small></h3>
-        <p class="item-description"><?php echo $description; if ( ! empty( $coach ) ) echo ' &mdash; '.$coach; ?></p>
+        <?php video_thumbnail( $img, $url ); ?>
+        <?php video_title( $title, $year, $url ); ?>
+        <?php video_description( $description, $coach ); ?>
       </li><?php
     endforeach;
     echo '</ul>';
   endforeach;
+}
+
+function video_thumbnail( $img, $url ) { ?>
+  <a href="<?php echo $url; ?>" target="_blank"><img class="item-thumbnail" src="<?php echo $img; ?>"></a><?php
+}
+
+function video_title( $title, $year, $url ) { ?>
+  <h3 class="item-title">
+    <a href="<?php echo $url; ?>" target="_blank"><?php echo $title; ?></a>
+    <?php if ( ! empty( $year ) ) echo '<small>' . $year . '</small>'; ?>
+  </h3><?php
+}
+
+function video_description( $description, $coach ) {
+  if ( ! empty( $description ) || ! empty( $coach ) ) echo '<p class="item-description">';
+  if ( ! empty( $description ) ) echo $description;
+  if ( ! empty( $coach ) ) echo ' &mdash; '.$coach;
+  if ( ! empty( $description ) || ! empty( $coach ) ) echo '</p>';
 }
