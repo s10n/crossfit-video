1aedd45ceccc4676aa37249a1e8311d069b9c77d
심철환 <a@akaiv.com>
2016-03-14 03:02:20 +0900

    컨텐츠 정리: 리프팅 및 WOD 비디오들에 메타 정보 추가

--- array-wod.php
+++ array-wod.php
@@ -1,32 +1,39 @@
 <?php
 $array['WOD'] = array(
+  // array(
+  //   '동작' => '',
+  //   '유튜브' => array(
+  //     // array( '제목' => "", '주소' => "", '연도' => "", '설명' => "", '코치' => "" ),
+  //   )
+  // ),
 
   array(
     '동작' => '크로스핏 오픈',
     '유튜브' => array(
-      array( '제목' => "15.1", '주소' => "0-YfYcsduHA" ),
-      array( '제목' => "15.2", '주소' => "DlsJ-70Pvwo" ),
-      array( '제목' => "15.3", '주소' => "-G6Fcm6rqCU" ),
-      array( '제목' => "15.4", '주소' => "sxuIbAkg4bU" ),
-      array( '제목' => "15.5", '주소' => "2c6pDE49g2k" ),
-      array( '제목' => "16.1", '주소' => "HtKb8Juxw9w" ),
-      array( '제목' => "16.2", '주소' => "_oJtaRP80Oo" ),
-      array( '제목' => "16.3", '주소' => "KggGsvxD3pY" ),
+      // array( '제목' => "", '주소' => "", '연도' => "", '설명' => "", '코치' => "" ),
+      array( '제목' => "15.1", '주소' => "0-YfYcsduHA", '연도' => "2015", '설명' => "<strong>9-MIN. AMRAP</strong><br>15 TOES-TO-BARS<br>10 DEADLIFTS<br>5 SNATCHES<br>&male; 115 lb. &female; 75 lb.<br><strong>WORKOUT 15.1a</strong><br>1-REP-MAX CLEAN AND JERK<br>6-minute time cap" ),
+      array( '제목' => "15.2", '주소' => "DlsJ-70Pvwo", '연도' => "2015", '설명' => "<strong>EVERY 3 MINUTES FOR AS LONG AS POSSIBLE:</strong><br><strong>FROM 0:00-3:00</strong><br><strong>2 ROUNDS OF:</strong><br>10 OVERHEAD SQUATS<br>10 CHEST-TO-BAR PULL-UPS<br><strong>FROM 3:00-6:00</strong><br><strong>2 ROUNDS OF:</strong><br>12 OVERHEAD SQUATS<br>12 CHEST-TO-BAR PULL-UPS<br><strong>FROM 6:00-9:00</strong><br><strong>2 ROUNDS OF:</strong><br>14 OVERHEAD SQUATS<br>14 CHEST-TO-BAR PULL-UPS<br>&male; 95 lb. &female; 65 lb.<br>Etc., following same pattern" ),
+      array( '제목' => "15.3", '주소' => "-G6Fcm6rqCU", '연도' => "2015", '설명' => "<strong>14-MIN. AMRAP</strong><br>7 MUSCLE-UPS<br>50 WALL BALLS<br>100 DOUBLE-UNDERS<br>&male; 20-lb. ball to 10 feet<br>&female; 14-lb. ball to 9 feet" ),
+      array( '제목' => "15.4", '주소' => "sxuIbAkg4bU", '연도' => "2015", '설명' => "<strong>8-MIN. AMRAP</strong><br>3 HSPU<br>3 CLEANS<br>6 HSPU<br>3 CLEANS<br>9 HSPU<br>3 CLEANS<br>12 HSPU<br>6 CLEANS<br>15 HSPU<br>6 CLEANS<br>18 HSPU<br>6 CLEANS<br>21 HSPU<br>9 CLEANS<br>&male; 185 lb. &female; 125 lb.<br>Etc., following same pattern" ),
+      array( '제목' => "15.5", '주소' => "2c6pDE49g2k", '연도' => "2015", '설명' => "<strong>FOR TIME:</strong><br>27-CALORIE ROW<br>27 THRUSTERS<br>21-CALORIE ROW<br>21 THRUSTERS<br>15-CALORIE ROW<br>15 THRUSTERS<br>9-CALORIE ROW<br>9 THRUSTERS<br>&male; 95 lb. &female; 65 lb." ),
+      array( '제목' => "16.1", '주소' => "HtKb8Juxw9w", '연도' => "2016", '설명' => "<strong>20-MIN. AMRAP</strong><br>25-FT. OVERHEAD WALKING LUNGE<br>8 BURPEES<br>25-FT. OVERHEAD WALKING LUNGE<br>8 CHEST-TO-BAR PULL-UPS<br>&male; 95 LB. &female; 65 LB." ),
+      array( '제목' => "16.2", '주소' => "_oJtaRP80Oo", '연도' => "2016", '설명' => "<strong>4-MIN. AMRAP + BONUS TIME:</strong><br>25 TOES-TO-BARS<br>50 DOUBLE-UNDERS<br>SQUAT CLEANS<span>*</span><br><span>*</span>ROUND 1 = 15 CLEANS<br>&male; 135 LB. &female; 85 LB." ),
+      array( '제목' => "16.3", '주소' => "KggGsvxD3pY", '연도' => "2016", '설명' => "<strong>7-MIN. AMRAP</strong><br>10 POWER SNATCHES<br>3 BAR MUSCLE-UPS<br>&male; 75 LB. &female; 55 LB." ),
     )
   ),
 
   array(
     '동작' => '선수들',
     '유튜브' => array(
-      array( '제목' => "Camille Leblanc-Bazinet - Amanda", '주소' => "3I2uC2l2CzE" ),
-      array( '제목' => "Sam Briggs - 4 on 1", '주소' => "okhWf_fjt1Y" ),
-      array( '제목' => "Dan Bailey - 4 on 1", '주소' => "YrKokRDSZYg" ),
-      array( '제목' => "Poipu", '주소' => "ggcpYIpugcg" ),
-      array( '제목' => "Brook Ence - Heavy Fran+", '주소' => "4GAv1iQ26UM" ),
-      array( '제목' => "Sam Briggs - J.T.", '주소' => "qjqCZkzci_4" ),
-      array( '제목' => "Sam Briggs - 30 muscle-ups", '주소' => "2NFDBXq3fWE" ),
-      array( '제목' => "Rich Froning - Oct 2, 2015", '주소' => "MX7j-lJz1qs" ),
-      array( '제목' => "Dan Bailey - Feb 8, 2016", '주소' => "0zCzaAFaYm8" ),
+      // array( '제목' => "", '주소' => "", '연도' => "", '설명' => "", '코치' => "" ),
+      array( '제목' => "Amanda", '주소' => "3I2uC2l2CzE", '연도' => 2014, '설명' => "<strong>FOR TIME:</strong><br>9-7-5<br>Muscle-ups<br>Squat snatches", '코치' => "Camille Leblanc-Bazinet" ),
+      array( '제목' => "4 on 1", '주소' => "okhWf_fjt1Y", '연도' => 2014, '설명' => "Isabel<br>Fran<br>Grace<br>Diane", '코치' => "Sam Briggs" ),
+      array( '제목' => "Heavy Fran+", '주소' => "4GAv1iQ26UM", '연도' => 2014, '설명' => "<strong>FOR TIME:</strong><br>21-15-9<br>Thrusters<br>Pull-ups", '코치' => "Brook Ence" ),
+      array( '제목' => "J.T.", '주소' => "qjqCZkzci_4", '연도' => 2015, '설명' => "<strong>FOR TIME:</strong><br>21-15-9<br>HSPU<br>Ring Dips<br>Push-ups", '코치' => "Sam Briggs" ),
+      array( '제목' => "30 muscle-ups", '주소' => "2NFDBXq3fWE", '연도' => 2015, '설명' => "30 muscle-ups in 3:03", '코치' => "Sam Briggs" ),
+      array( '제목' => "2015년 10월 2일", '주소' => "MX7j-lJz1qs", '연도' => 2015, '설명' => "<strong>7-MIN. AMRAP</strong><br>2-4-6-8-10-12-14...<br>Deadlifts<br>Strict HSPU<br>Rest 5 minutes then<br><strong>7-MIN. AMRAP</strong><br>2-4-6-8-10-12-14...<br>Power cleans<br>Pistols", '코치' => "Rich Froning" ),
+      array( '제목' => "2016년 2월 8일", '주소' => "0zCzaAFaYm8", '연도' => 2016, '설명' => "<strong>3 ROUNDS</strong><br>Max reps at each station<br><strong>0:00-2:00</strong><br>Run 400 meters<br>Double-unders<br><strong>2:00-3:00</strong><br>Dumbbell Push Jerk<br><strong>3:00-4:00</strong><br>Pull-ups<br><strong>4:00-5:00</strong><br>Dumbbell Hang Squat Cleans<br>Rest 5 minutes between rounds", '코치' => "Dan Bailey" ),
+      array( '제목' => "Saturday Workout", '주소' => "ggcpYIpugcg", '연도' => 2014, '코치' => "CrossFit Poipu" ),
     )
   ),
 
