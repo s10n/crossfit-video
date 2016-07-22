b7d6d644199c95f192b7917a0b1a596ba64abf3e
심철환 <a@akaiv.com>
2016-07-22 15:05:25 +0900

    컨텐츠 추가

--- array-workout.php
+++ array-workout.php
@@ -12,6 +12,7 @@ $array['운동'] = array(
     '유튜브' => array(
       // array( '제목' => "", '주소' => "", '연도' => "", '설명' => "", '코치' => "" ),
       array( '제목' => "웜업", '주소' => "sI8h1xNUxcE", '연도' => 2013, '코치' => "Matt Chan" ),
+      array( '제목' => "웜업", '주소' => "VW42KKmjEZY", '연도' => 2016, '코치' => "Chris Hinshaw" ),
       array( '제목' => "햄스트링 활성화 드릴", '주소' => "JwE3Qjh04Xo", '연도' => 2016, '코치' => "PJ Newton " ),
     )
   ),
@@ -28,6 +29,8 @@ $array['운동'] = array(
       array( '제목' => "16.1", '주소' => "HtKb8Juxw9w", '연도' => "2016", '설명' => "<strong>20-MIN. AMRAP</strong><br>25-FT. OVERHEAD WALKING LUNGE<br>8 BURPEES<br>25-FT. OVERHEAD WALKING LUNGE<br>8 CHEST-TO-BAR PULL-UPS<br>&male; 95 LB. &female; 65 LB." ),
       array( '제목' => "16.2", '주소' => "_oJtaRP80Oo", '연도' => "2016", '설명' => "<strong>4-MIN. AMRAP + BONUS TIME:</strong><br>25 TOES-TO-BARS<br>50 DOUBLE-UNDERS<br>SQUAT CLEANS<span>*</span><br><span>*</span>ROUND 1 = 15 CLEANS<br>&male; 135 LB. &female; 85 LB." ),
       array( '제목' => "16.3", '주소' => "KggGsvxD3pY", '연도' => "2016", '설명' => "<strong>7-MIN. AMRAP</strong><br>10 POWER SNATCHES<br>3 BAR MUSCLE-UPS<br>&male; 75 LB. &female; 55 LB." ),
+      array( '제목' => "16.4", '주소' => "w9LHMzGR1hg", '연도' => "2016", '설명' => "<strong>13-MIN. AMRAP</strong><br>55 DEADLIFTS<br>55 WALL-BALL SHOTS<br>55-CALORIE ROW<br>55 HANDSTAND PUSH-UPS<br>&male; 225 LB. / 20-LB. BALL<br>&female; 155 LB. / 14-LB. BALL" ),
+      array( '제목' => "16.5", '주소' => "JLdfEkTJ2Nw", '연도' => "2016", '설명' => "<strong>FOR TIME:</strong><br>21-18-15-12-9-6-3<br>THRUSTERS<br>BURPEES<br>&male; 95 LB. &female;  65 LB." ),
     )
   ),
 
@@ -35,6 +38,7 @@ $array['운동'] = array(
     '동작' => '선수들',
     '유튜브' => array(
       // array( '제목' => "", '주소' => "", '연도' => "", '설명' => "", '코치' => "" ),
+      array( '제목' => "Isabel", '주소' => "7iUxoTlt5aw", '연도' => 2016, '설명' => "<strong>FOR TIME:</strong><br>30 Snatches", '코치' => "Emily Abbott" ),
       array( '제목' => "Amanda", '주소' => "3I2uC2l2CzE", '연도' => 2014, '설명' => "<strong>FOR TIME:</strong><br>9-7-5<br>Muscle-ups<br>Squat snatches", '코치' => "Camille Leblanc-Bazinet" ),
       array( '제목' => "4 on 1", '주소' => "okhWf_fjt1Y", '연도' => 2014, '설명' => "Isabel<br>Fran<br>Grace<br>Diane", '코치' => "Sam Briggs" ),
       array( '제목' => "Heavy Fran+", '주소' => "4GAv1iQ26UM", '연도' => 2014, '설명' => "<strong>FOR TIME:</strong><br>21-15-9<br>Thrusters<br>Pull-ups", '코치' => "Brook Ence" ),
@@ -46,7 +50,46 @@ $array['운동'] = array(
       array( '제목' => "2016년 2월 16일", '주소' => "lBoqyh55n54", '연도' => 2016, '설명' => "<strong>FOR TIME:</strong><br>90 Double-unders<br>9 Muscle-ups<br>9 Squat clean thrusters<br>80 Double-unders<br>8 Muscle-ups<br>8 Squat clean thrusters<br>70 Double-unders<br>7 Muscle-ups<br>7 Squat clean thrusters", '코치' => "Julie Foucher" ),
       array( '제목' => "7분 머슬업", '주소' => "VQyTWhnHsGw", '연도' => 2016 ),
       array( '제목' => "2016년 2월 23일", '주소' => "FY2jIlaFN44", '연도' => 2016, '설명' => "<strong>5 ROUNDS</strong><br>12 Dumbbell snatches<br>9 Bench presses<br>6 Bar muscle-ups", '코치' => "Ben Smith" ),
+      array( '제목' => "2016년 3월 21일", '주소' => "FO8EG6E3L4w", '연도' => 2016, '설명' => "<strong>For time:</strong><br>42 Medicine-ball cleans<br>21 Body-weight bench presses <br>42 Medicine-ball cleans<br>15 Body-weight bench presses <br>42 Medicine-ball cleans<br>9 Body-weight bench presses", '코치' => "Dan Bailey" ),
       array( '제목' => "Saturday Workout", '주소' => "ggcpYIpugcg", '연도' => 2014, '코치' => "CrossFit Poipu" ),
+    )
+  ),
+
+  array(
+    '동작' => '게임즈',
+    '유튜브' => array(
+      // array( '제목' => "", '주소' => "", '연도' => "", '설명' => "", '코치' => "" ),
+      array( '제목' => "개썰매: 2011 남자", '주소' => "Hd6rTwqjqks", '연도' => 2014 ),
+      array( '제목' => "개썰매: 2011 여자", '주소' => "WIDKj_lw7Ho", '연도' => 2014 ),
+      array( '제목' => "푸시 풀 개인전", '주소' => "ydIzpekJBNU", '연도' => 2014 ),
+      array( '제목' => "지그재그 스프린트: 여자 2013", '주소' => "dND46MnECgE", '연도' => 2014 ),
+    )
+  ),
+
+  array(
+    '동작' => '동기부여',
+    '유튜브' => array(
+      // array( '제목' => "", '주소' => "", '연도' => "", '설명' => "", '코치' => "" ),
+      array( '제목' => "Be More Human", '주소' => "9ltYbltoe1E", '연도' => 2015, '코치' => "리복" ),
+      array( '제목' => "너를 외쳐봐", '주소' => "ONLW-q4S8Gg", '연도' => 2015, '코치' => "나이키" ),
+    )
+  ),
+
+  array(
+    '동작' => '동기부여 &mdash; 아디다스 커머셜',
+    '유튜브' => array(
+      // array( '제목' => "", '주소' => "", '연도' => "", '설명' => "", '코치' => "" ),
+      array( '제목' => "줄리", '주소' => "gtPP9L2qriY", '연도' => 2016, '코치' => "러닝" ),
+      array( '제목' => "모건 <i class=\"fa fa-fw fa-star\"></i>", '주소' => "n4nHwk0a6-4", '연도' => 2016, '코치' => "축구" ),
+      array( '제목' => "칼리 <i class=\"fa fa-fw fa-star\"></i><i class=\"fa fa-fw fa-star\"></i>", '주소' => "uyND-bFTSNI", '연도' => 2016, '코치' => "슈퍼모델" ),
+      array( '제목' => "아나", '주소' => "gdt5g-_9Q7Q", '연도' => 2016, '코치' => "테니스" ),
+      array( '제목' => "애드리언", '주소' => "dp8oc8PIYyw", '연도' => 2016, '코치' => "요가" ),
+      array( '제목' => "사샤", '주소' => "zTsaqLs7cd0", '연도' => 2016, '코치' => "클라이밍" ),
+      array( '제목' => "니콜 윈호퍼", '주소' => "Nar3e9e4-nI", '연도' => 2016, '코치' => "피트니스아티스트" ),
+      array( '제목' => "캔다스 파커", '주소' => "migK7V709OQ", '연도' => 2016, '코치' => "농구" ),
+      array( '제목' => "한나 브로프만", '주소' => "NNfkrq_gKlo", '연도' => 2016, '코치' => "피트니스" ),
+      array( '제목' => "로빈 <i class=\"fa fa-fw fa-star\"></i>", '주소' => "2XZYWXXqz0o", '연도' => 2016, '코치' => "피트니스" ),
+      array( '제목' => "캐롤라인 <i class=\"fa fa-fw fa-star\"></i><i class=\"fa fa-fw fa-star\"></i>", '주소' => "MBV7Z2njneQ", '연도' => 2016, '코치' => "테니스" ),
     )
   ),
 
