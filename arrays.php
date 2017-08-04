2079595886019e819e0634641590b000bfbba7ca
심철환 <a@akaiv.com>
2017-08-04 20:47:59 +0900

    Remove all legacy files

--- arrays.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'array-foundation.php';
-include 'array-gymnastics.php';
-include 'array-lifting.php';
-include 'array-workout.php';
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'arrays.php';
-include 'functions.php';
-include 'header.php';
-include 'contents.php';
-include 'footer.php'; ?>
